c38c025c5a6bcdc48b49b30d03573c6577d9bcca
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-03-23 16:17:43 +0100

    Add requirements and edit main.py 😊

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Advanced Algorithms - Lab1
 
 Advanced Algorithms UniPD - Lab 1
+
+
+## TODOs
+
+- [] Unico argomento per decidere quale algoritmo l'utente vuole utilizzare 
+- [] 3 Funzioni con input e output ciascuna che implementa l'algoritmo
+- [] (opz) Secondo argomento per indicare la cartella di output del grafico
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Module Docstring
 """
@@ -7,12 +8,69 @@ __author__ = "E. Buratto, M. Sciacco"
 __version__ = "0.0.1"
 __license__ = "Unlicense"
 
+import argparse
+from random import randint
+import gc
+from time import perf_counter_ns
+import matplotlib.pyplot as plt
 
-def main():
+
+# TODO: adapt the code to the one we need
+
+def measure_run_time(list_size, num_calls, num_instances):
+  sum_times = 0.0
+  for i in range(num_instances):
+    alist = [randint(0,10000) for i in range(list_size)]
+    target = randint(0,10000)
+    gc.disable()
+    start_time = perf_counter_ns()
+    for i in range(num_calls):
+      print(i)
+      # linear_search(alist, target)
+    end_time = perf_counter_ns()
+    gc.enable()
+    sum_times += (end_time - start_time)/num_calls
+  avg_time = int(round(sum_times/num_instances))
+  # return average time in nanoseconds
+  return avg_time
+
+avg_time = measure_run_time(10, 100000, 4)
+print("Average time (ns):", avg_time)
+
+
+
+def main(args):
     """ Main entry point of the app """
     print("hello world")
+    print(args)
 
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    main()
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("arg", help="Required positional argument")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--name", action="store", dest="name")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)

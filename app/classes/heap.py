915398b55f87d65ca96db597ca7a841124e1c63a
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-04-02 10:16:18 +0200

    Update heap.py

--- app/classes/heap.py
+++ app/classes/heap.py
@@ -4,13 +4,25 @@
 """
 	Heap data structure
 	-----------------------------------------
-"""
+"""s
+
+class node : 
+
+    def __init__(self, parent, index):
+		self.parent = parent
+        self.index = index
+        self.children = []
+    
+    def addChild(self, child):
+        self.children.append(child)
+
+
 
 class heap :
 
 	def __init__(self):
-		self.lastData = "none"
-
+		self.root = Nan
+        self. 
 
 	def response(self) :
 		output = self.usb.readline().decode('utf-8').rstrip()

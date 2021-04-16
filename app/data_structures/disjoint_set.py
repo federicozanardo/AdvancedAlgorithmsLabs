b4eb61766a0064dc6e9053c8e32759c3415648ae
Federico Zanardo <federicozanardo98@gmail.com>
2021-04-16 17:30:24 +0200

    Update the union-find data structure.

--- app/data_structures/disjoint_set.py
+++ app/data_structures/disjoint_set.py
@@ -1,37 +1,33 @@
+import math
 import sys
 sys.path.append('../')
 
-class DisjointSet:
-    def __init__(self, n):
-        self.nodes = [DisjointSetNode] * n
 
-    def make_set(self, value: int):
-        x = DisjointSetNode()
-        x.value = value
-        x.parent = x
-        x.size = 1
-        self.nodes[value - 1] = x
+class DisjointSet:
+    def __init__(self):
+        self.parents = [math.inf]
+        self.sizes = [0]
+
+    def make_set(self, x: int):
+        self.parents.append(x)
+        self.sizes.append(1)
 
     def find_set(self, x: int):
-        if self.nodes[x - 1].parent != self.nodes[x - 1]:
-            self.nodes[x - 1].parent = self.find_set(self.nodes[x - 1].parent.value)
-            return self.nodes[x - 1].parent
-        else:
-            return self.nodes[x - 1]
+        root = x
+
+        while self.parents[root] != root:
+            root = self.parents[root]
+
+        return root
 
     def union_by_size(self, x: int, y: int):
-        x = self.find_set(x)
-        y = self.find_set(y)
+        i = self.find_set(x)
+        j = self.find_set(y)
 
-        if x != y:
-            if x.size < y.size:
-                (x, y) = (y, x)
-
-            y.parent = x
-            x.size += y.size
-
-
-class DisjointSetNode:
-    value = None
-    parent = None
-    size = None
+        if i != j:
+            if self.sizes[i] >= self.sizes[j]:
+                self.parents[j] = i
+                self.sizes[i] += self.sizes[j]
+            else:
+                self.parents[i] = j
+                self.sizes[j] += self.sizes[i]

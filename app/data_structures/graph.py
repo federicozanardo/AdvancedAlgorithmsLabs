fb3f1d53127052a5c664e3d7c5cf30ea5adff8ff
Federico Zanardo <federicozanardo98@gmail.com>
2021-04-20 22:52:11 +0200

    Update graph data structure.

--- app/data_structures/graph.py
+++ app/data_structures/graph.py
@@ -10,12 +10,17 @@ class Graph:
         self.V = set()
         self.E = []
 
-    def add_vertex(self, value):
+    def add_vertex(self, value: int):
         self.V.add(value)
         self.graph[value] = []
 
     # assunzione: viene chiamato appena dopo gli addVertex
-    def add_edge(self, u, v, w):
+    def add_edge(self, u: int, v: int, w: int):
         self.E.append((u, v, w))
         self.graph[u].append((v, w))
         self.graph[v].append((u, w))
+
+    def remove_edge(self, u: int, v: int, w: int):
+        self.E.remove((u, v, w))
+        self.graph[u].remove((v, w))
+        self.graph[v].remove((u, w))

0114907240bbfb5490111fe2ed5b5532230d8827
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-04-23 14:13:05 +0200

    Update prim.py

--- app/algorithms/prim.py
+++ app/algorithms/prim.py
@@ -75,197 +75,134 @@ sys.path.append('../')
 #         return sum
         
 
-  
-class Heap():
-  
-    def __init__(self):
-        self.array = []
-        self.size = 0
-        self.pos = []
-  
-    def newMinHeapNode(self, v, dist):
-        minHeapNode = [v, dist]
-        return minHeapNode
-  
-    # A utility function to swap two nodes of 
-    # min heap. Needed for min heapify
-    def swapMinHeapNode(self, a, b):
-        t = self.array[a]
-        self.array[a] = self.array[b]
-        self.array[b] = t
-  
-    # A standard function to heapify at given idx
-    # This function also updates position of nodes 
-    # when they are swapped. Position is needed 
-    # for decreaseKey()
-    def minHeapify(self, idx):
-        smallest = idx
-        left = 2 * idx + 1
-        right = 2 * idx + 2
-  
-        if left < self.size and self.array[left][1] < \
-                                self.array[smallest][1]:
-            smallest = left
-  
-        if right < self.size and self.array[right][1] < \
-                                self.array[smallest][1]:
-            smallest = right
-  
-        # The nodes to be swapped in min heap 
-        # if idx is not smallest
-        if smallest != idx:
-  
-            # Swap positions
-            self.pos[ self.array[smallest][0] ] = idx
-            self.pos[ self.array[idx][0] ] = smallest
-  
-            # Swap nodes
-            self.swapMinHeapNode(smallest, idx)
-  
-            self.minHeapify(smallest)
-  
-    # Standard function to extract minimum node from heap
-    def extractMin(self):
-  
-        # Return NULL wif heap is empty
-        if self.isEmpty() == True:
-            return
-  
-        # Store the root node
-        root = self.array[0]
-  
-        # Replace root node with last node
-        lastNode = self.array[self.size - 1]
-        self.array[0] = lastNode
-  
-        # Update position of last node
-        self.pos[lastNode[0]] = 0
-        self.pos[root[0]] = self.size - 1
-  
-        # Reduce heap size and heapify root
-        self.size -= 1
-        self.minHeapify(0)
-  
-        return root
-  
-    def isEmpty(self):
-        return True if self.size == 0 else False
-  
-    def decreaseKey(self, v, dist):
-  
-        # Get the index of v in  heap array
-  
-        i = self.pos[v]
-  
-        # Get the node and update its dist value
-        self.array[i][1] = dist
-  
-        # Travel up while the complete tree is not 
-        # hepified. This is a O(Logn) loop
-        while i > 0 and self.array[i][1] < self.array[(i - 1) // 2][1]:
-  
-            # Swap this node with its parent
-            self.pos[ self.array[i][0] ] = (i-1)//2
-            self.pos[ self.array[(i-1)//2][0] ] = i
-            self.swapMinHeapNode(i, (i - 1)//2 )
-  
-            # move to parent index
-            i = (i - 1) // 2
-  
-    # A utility function to check if a given vertex
-    # 'v' is in min heap or not
-    def isInMinHeap(self, v):
-        for x in self.pos:
-            if x == v:
-                return True
-        #if self.pos[v] < self.size:
-         #   return True
-        return False
-  
-  
-class Prim():
-  
-    def __init__(self):
-        self.graph = defaultdict(list)
-  
-    # Adds an edge to an undirected graph
-    def addEdge(self, src, dest, weight):
-  
-        # Add an edge from src to dest.  A new node is
-        # added to the adjacency list of src. The node 
-        # is added at the beginning. The first element of
-        # the node has the destination and the second 
-        # elements has the weight
-        newNode = [dest, weight]
-        self.graph[src].insert(0, newNode)
-  
-        # Since graph is undirected, add an edge from 
-        # dest to src also
-        newNode = [src, weight]
-        self.graph[dest].insert(0, newNode)
-  
-    # The main function that prints the Minimum 
-    # Spanning Tree(MST) using the Prim's Algorithm. 
-    # It is a O(ELogV) function
-    def prim_mst(self, G, s):
+from collections import defaultdict
+import sys
 
-        self.graph = G.graph
-        # Get the number of vertices in graph
-        V = len(G.V)
-          
-        # key values used to pick minimum weight edge in cut
-        key = []   
-          
-        # List to store contructed MST
-        parent = [] 
-  
-        # minHeap represents set E
-        minHeap = Heap()
-  
-        # Initialize min heap with all vertices. Key values of all
-        # vertices (except the 0th vertex) is is initially infinite
-        for v in range(V):
-            parent.append(-1)
-            key.append(float('inf'))
-            minHeap.array.append( minHeap.newMinHeapNode(v, key[v]) )
-            minHeap.pos.append(v)
-  
-        # Make key value of 0th vertex as 0 so 
-        # that it is extracted first
-        minHeap.pos[0] = 0
-        key[0] = 0
-        minHeap.decreaseKey(0, key[0])
-  
-        # Initially size of min heap is equal to V
-        minHeap.size = V
-  
-        # In the following loop, min heap contains all nodes
-        # not yet added in the MST.
-        while minHeap.isEmpty() == False:
-  
-            # Extract the vertex with minimum distance value
-            newHeapNode = minHeap.extractMin()
-            u = newHeapNode[0]
-  
-            # Traverse through all adjacent vertices of u 
-            # (the extracted vertex) and update their 
-            # distance values
-            for pCrawl in self.graph[u]:
-                print(pCrawl)
-                print(minHeap.size)
-                v = pCrawl[0]
-  
-                # If shortest distance to v is not finalized 
-                # yet, and distance to v through u is less than
-                # its previously calculated distance
-                if minHeap.isInMinHeap(v) and pCrawl[1] < key[v]:
-                    key[v] = pCrawl[1]
-                    parent[v] = u
-  
-                    # update distance value in min heap also
-                    minHeap.decreaseKey(v, key[v])
-  
-        return key
+class HeapNode():
+
+    def __init__(self,n,w): 
+        self.node = n
+        self.weight = w
+
+class MinHeap():
+
+    def __init__(self): 
+        self.heap = []
+        self.areInHeap = defaultdict(list)
+
+    def getMin(self):
+        return self.heap[0]
+
+    def insert(self, node):
+        self.heap.append(node)
+        position = self.heapifyUp(len(self.heap)-1)
+        self.areInHeap[node.node] = position # TO CHECK, NEED TO SET 2 ELEMENTS
+
+    def heapifyUp(self, index) :
+        while(index > 0):
+            element = self.heap[index]
+            parentIndex = (index-1) // 2
+            parent = self.heap[parentIndex]
+            # print(element.weight, "parent weight = ", parent.weight)
+            if parent.weight <= element.weight:
+                break
+
+            self.areInHeap[(self.heap[parentIndex].node, index)]
+            self.heap[index] = parent
+            self.heap[parentIndex] = element
+            index = parentIndex
+        return index
+
+    def extractMin(self):
+        minimum = self.heap[0]
+        lastElement = self.heap.pop(len(self.heap)-1)
+
+        if(minimum != None):
+            del self.areInHeap[minimum.node]
+        
+        if len(self.heap) != 0:
+            self.heap[0] = lastElement
+            self.heapifyDown(0)
+
+        return minimum
+
+    def heapifyDown(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        smallest = index
+        length = len(self.heap)
+
+        print(left, right, smallest, length)
+
+        if left <= length and smallest <= length:
+            if self.heap[left] and self.heap[left].weight < self.heap[smallest].weight:
+                smallest = left
+        if right <= length and smallest <= length:
+            if self.heap[right] and self.heap[right].weight < self.heap[smallest].weight:
+                smallest = right
+        self.areInHeap[self.heap[smallest].node] = index # TO CHECK
+        self.areInHeap[self.heap[index].node] = smallest # TO CHECK
+
+        if smallest != index:
+            self.heap[smallest], self.heap[index] = self.heap[index],self.heap[smallest]
+            self.heapifyDown(smallest)
+
+    def contains(self, node) :
+        return node in self.areInHeap
+
+    def update(self, node, weight):
+        index = self.areInHeap.get(node)
+        self.heap[index].weight = float('-inf')
+        self.heapifyUp(index)
+        self.heap[0].weight = weight
+        self.heapifyDown(0)
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+
+def initialization(graph):
+    key = defaultdict(list)
+    parents = defaultdict(list)
+    nodes = graph.V
+
+    for node in nodes:
+        key[node] = float('inf')
+        parents = [node], None
+
+    return key, parents
+
+
+def createPriorityQueue(graph, key):
+    minHeap = MinHeap()
+    nodes = graph.V # same as: graph.graph.keys()
+
+    for node in nodes:
+        minHeap.insert(HeapNode(node, key[node]))
+
+    return minHeap
+
+
+class Prim():
+
+    def prim_mst(self, graph, startingNode) :
+        key, parents = initialization(graph)
+        key[startingNode] = 0
+
+        priorityQueue = createPriorityQueue(graph, key)
+
+        while not priorityQueue.isEmpty():
+            current = priorityQueue.extractMin()
+            adjacentOfCurrent = graph.graph[current.node]
+
+            for adjNode in adjacentOfCurrent:
+                if priorityQueue.contains(adjNode) and graph.weightBetween(current.node, adjNode[0]) < key[adjNode[0]]:
+                    parents[adjNode[0]] = current
+                    key[adjNode[0]] = graph.weightBetween(current.node, adjNode[0])
+                    priorityQueue.update(adjNode[0], key[adjNode[0]])
+        return key, parents
+    
+
 
     def get_weight(self, key):
         sum = 0

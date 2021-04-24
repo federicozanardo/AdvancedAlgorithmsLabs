7ebe29ee51697066e472c6ada7ddfa1776535671
enricobu96 <enrico.buratto.1@studenti.unipd.it>
2021-04-24 22:51:32 +0200

    forse prim più veloce

--- app/data_structures/heap.py
+++ app/data_structures/heap.py
@@ -1,6 +1,7 @@
 #!/user/bin/env python3
 
 import sys
+from collections import defaultdict
 sys.path.append('../')
 """
     Heap (binary)
@@ -34,6 +35,7 @@ class Heap:
 
     def __init__(self):
         self.list = [Node(0, float('-inf'))] # nodo fittizio per avere array che partono da 1
+        self.mapList = defaultdict(list)
         self.currentSize = 0
     
     """
@@ -65,21 +67,40 @@ class Heap:
         index = indice del nodo da cui eseguire heapify
     Esegue la procedura heapify dello heap dal basso verso l´alto
     """
-    def heapifyUp(self, index): 
+    def heapifyUp(self, index):
+        # while(index>0):
+        #     el = self.list[index]
+        #     pIndex = index // 2
+        #     parent = self.list[pIndex]
+        #     if parent.weight < el.weight:
+        #         break
+            
+        #     self.mapList[self.list[pIndex].index] = index
+        #     self.list[index] = parent
+        #     self.list[pIndex] = el
+        #     index = pIndex
+        # return index 
+
+    # def heapifyUp(self, index):
+        constInd = index
         while index // 2 > 0:
             if self.list[index].weight < self.list[index // 2].weight:
+                self.mapList[self.list[(index)].index] = index // 2
+                self.mapList[self.list[index // 2].index] = index
+                constInd = index // 2
                 self.list[index], self.list[index // 2] = self.list[index // 2], self.list[index]
             index //= 2
-
+        return constInd
+    
+    
     """
     search(index: int) : boolean
         index = indice del nodo da cercare
     Restituisce True se il nodo di indice index è presente nello heap, altrimenti restituisce false
     """
     def search(self, index):
-        for node in self.list:
-            if node.index == index:
-                return True
+        if index in self.mapList.keys():
+            return True
         return False
     
     """
@@ -99,19 +120,28 @@ class Heap:
             aggiorna l´indice a cui cercare il nodo
     """
     def searchAndUpdateWeight(self, index, newWeight):
-        i = 0
+        print(index)
+        i = self.mapList[index]
         for node in self.list:
-            if i == 0:
-                i += 1
-                continue
-            if node.index == index:
-                self.list[i].weight = float('-inf')
-                self.heapifyUp(i)
-                self.list[1].weight = newWeight
-                self.heapifyDown(1)
-                return
-            else: 
-                i += 1
+            print(node.toTuple())
+        self.list[i].weight = float('-inf')
+        self.heapifyUp(i)
+        self.list[1].weight = newWeight
+        self.heapifyDown(1)
+
+        # i = 0
+        # for node in self.list:
+        #     if i == 0:
+        #         i += 1
+        #         continue
+        #     if node.index == index:
+        #         self.list[i].weight = float('-inf')
+        #         self.heapifyUp(i)
+        #         self.list[1].weight = newWeight
+        #         self.heapifyDown(1)
+        #         return
+        #     else: 
+        #         i += 1
             
     """
     insert(node: Node) : void
@@ -121,7 +151,8 @@ class Heap:
     def insert(self, node):
         self.list.append(node)
         self.currentSize += 1
-        self.heapifyUp(self.currentSize)
+        pos = self.heapifyUp(self.currentSize)
+        self.mapList[node.index] = pos
     
     """
     heapifyDown(index: int) : void
@@ -129,9 +160,30 @@ class Heap:
     Esegue la procedura heapify dello heap dall´alto verso il basso
     """
     def heapifyDown(self, index):
+        # left = 2 * index
+        # right = 2 * index +1
+        # smallest = index
+        # length = self.currentSize
+
+        # if(left <= length and self.list[left] and self.list[left].weight < self.list[smallest].weight):
+        #     smallest = left
+
+        # if(right <= length and self.list[right] and self.list[right].weight < self.list[smallest].weight):
+        #     smallest = right
+
+        # self.mapList[self.list[smallest].index] = index
+        # self.mapList[self.list[index].index] = smallest
+
+        # if smallest != index:
+        #     self.list[smallest], self.list[index] = self.list[index], self.list[smallest]
+
+        # self.heapifyDown(smallest)
+
         while (index * 2) <= self.currentSize :
             minChild = self.minChild(index)
             if self.list[index].weight > self.list[minChild].weight: 
+                self.mapList[self.list[minChild].index] = index
+                self.mapList[self.list[index].index] = minChild
                 self.list[index], self.list[minChild] = self.list[minChild], self.list[index]
             index = minChild
 
@@ -168,7 +220,9 @@ class Heap:
         if len(self.list) == 1:
             return None
         minEl = self.list[1]
+        del self.mapList[minEl.index]
         self.list[1] = self.list[self.currentSize]
+        self.mapList[self.list[self.currentSize].index] = 1
         # *self.list, _ = self.list
         del self.list[self.currentSize]
         self.currentSize -= 1

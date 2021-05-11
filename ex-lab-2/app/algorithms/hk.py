7ec13d48e113e0a8b295af65b87a889b7e5b0950
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-05-11 18:10:14 +0200

    Edit HK for adjacent matrix

--- ex-lab-2/app/algorithms/hk.py
+++ ex-lab-2/app/algorithms/hk.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+import copy
 import sys
 sys.path.append('../')
 from data_structures.graph import Graph
+from data_structures.tsp import TSP
 from data_structures.heap import Heap, Node
 
 
@@ -11,34 +13,40 @@ class HeldKarp:
     def __init__(self):
         self.d = defaultdict(list)
         self.p = defaultdict(list)
+        self.tsp = None
 
     # graph = struttura dati del grafo
-    def hk_init(self, graph):
-        # TODO: deep copy time? 
-        return self.hk_visit(0, graph.graph)
+    def hk_init(self, tsp):
+        self.tsp = copy.deepcopy(tsp)
+        S = []
+        for i in range(1, self.tsp.dimension+1):
+            S.append(i)
+            self.d[i].append(None)
+            self.p[i].append(None)
+        return self.hk_visit(1, S)
 
     # PRE: S sottoinsieme di V, v € S
     # POST: d[v,S] è il peso / distanza del cammino minimo da 0 a v che visita tutti i vertici in S, p[v,S] è il predecessore di v
     # S = vertici da scorrere presenti in V
     # v = vertice corrente
     def hk_visit(self, v, S):
+        print("Visiting: ", v, "Remaining: ", len(S))
         if len(S) == 1 and v in S: # S[v] != null
-            (_,w) = S[v]
-            return w
-        elif v in self.d: # d[v] != null
-            return self.d[v]
+            return self.tsp.get_weight(v, 1)
+        elif v in self.d and self.d[v][0] != None: # d[v] != null
+            return self.d[v][0]
         else:
             mindist = float('inf')
             minprec = None
         
-        # FIXME: many doubts about the legality of this move
-            (u, w) = S[v]
-            S[v].remove((u, w))
-            for (u, w) in S:
+            # FIXME: many doubts about the legality of this move
+            S.remove(v)
+            for u in S:
                 dist = self.hk_visit(u, S)
+                w = self.tsp.get_weight(u, v)
                 if dist + w < mindist:
                     mindist = dist + w
                     minprec = u
-            self.d[v] = mindist
-            self.p[v] = minprec
+            self.d[v][0] = mindist
+            self.p[v][0] = minprec
             return mindist

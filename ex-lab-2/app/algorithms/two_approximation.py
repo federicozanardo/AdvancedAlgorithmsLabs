0f707eef6eac88e718f90e35ee8dd5998249e635
enricobu96 <enrico.buratto96@gmail.com>
2021-05-11 19:40:30 +0200

    non funziona™

--- ex-lab-2/app/algorithms/two_approximation.py
+++ ex-lab-2/app/algorithms/two_approximation.py
@@ -1,12 +1,13 @@
 from algorithms.prim import Prim
-from data_structures.graph import Graph
+from data_structures.tsp import TSP
 
 
 class TwoApproximation:
-    def algorithm(self, graph: Graph):
+    def algorithm(self, graph: TSP):
         starting_node = 1
         prim = Prim()
         key, mst = prim.prim_mst(graph, starting_node)
+        print('Result of Prim: ', mst)
 
         tree = {}
 
@@ -15,29 +16,32 @@ class TwoApproximation:
         for index in mst:
             if mst[index] != None:
                 (parent, w) = mst[index]
+                print('mst[index]: ', mst[index])
                 if parent not in tree:
                     tree[parent] = []
                 tree[parent].append(index)
 
-        print('Tree')
-        print(tree)
-        print()
+        print('Tree: {}\n'.format(tree))
 
         preorder_result = []
         sum = self.preorder(graph, tree, starting_node, preorder_result)
-
+        print('Result of preorder: ', preorder_result)
+        
         # Add the weight of the edge between the starting node and the last node of the MST to create a cycle
-        sum += int(graph.get_weight(preorder_result[len(preorder_result) - 1], 1))
-
+        #sum += int(graph.get_weight(preorder_result[len(preorder_result) - 1], 1))
+        sum += int(graph.adjMatrix[preorder_result[len(preorder_result) - 1]][1])
+        
         print('Result: {}'.format(str(sum)))
 
+        return sum
+
     # Complexity: teta(n)
-    def preorder(self, graph: Graph, tree, v, result):
+    def preorder(self, graph: TSP, tree, v, result):
         if v not in result:
             result.append(v)
         if v in tree:
             for u in tree[v]:
-                return self.preorder(graph, tree, u, result) + int(
-                    graph.get_weight(v, u))  # THE GRAPH MUST USE THE ADJACENCY MATRIX TO HAVE THIS TIME COMPLEXITY!
+                #graph.get_weight(v, u))  # THE GRAPH MUST USE THE ADJACENCY MATRIX TO HAVE THIS TIME COMPLEXITY!
+                return self.preorder(graph, tree, u, result) + int(graph.adjMatrix[v][u])
         else:
             return 0
--- ex-lab-2/app/data_structures/tsp.py
+++ ex-lab-2/app/data_structures/tsp.py
@@ -12,29 +12,41 @@ class TSP:
         self.nodes = defaultdict(list)
         self.adjMatrix = []
 
+
     def add_node(self, i: int, x: float, y: float):
         if(self.etype == 'GEO'):
-            degX, degY = int(x), int(y)
+            degX, degY = int(math.floor(x)), int(math.floor(y))
             minX, minY = x - degX, y - degY
-            x, y = math.pi*(degX+5*minX/3)/180.0, math.pi*(degY+5*minY/3)/180.0
+            x, y = math.pi*(degX+5.0*minX/3.0)/180.0, math.pi*(degY+5.0*minY/3.0)/180.0
         self.nodes[i] = [x,y]
 
-    def get_weight(self, first: int, sec: int):
-        RRR = 6378.388
-        q1 = math.cos(self.nodes[first][1] - self.nodes[sec][1])
-        q2 = math.cos(self.nodes[first][0] - self.nodes[sec][0])
-        q3 = math.cos(self.nodes[first][0] + self.nodes[sec][0])
-        final = RRR*math.acos(0.5*((1.0+q1)*q2 - (1.0-q1)*q3)) + 1.0
-        if self.etype == 'GEO':
-            return math.floor(final)
-        else:
-            return int(round(final))
-        
-    def calculateAdjMatrix(self):
-        for i in range(1, self.dimension+1):
-            self.adjMatrix.append([0 for i in range(1, self.dimension+1)])
+    # FUNZIONA (tm) ?
+    # def get_weight(self, first: int, sec: int):
+    #     RRR = 6378.388
+    #     q1 = math.cos(self.nodes[first][1] - self.nodes[sec][1])
+    #     q2 = math.cos(self.nodes[first][0] - self.nodes[sec][0])
+    #     q3 = math.cos(self.nodes[first][0] + self.nodes[sec][0])
+    #     final = (RRR*math.acos((0.5*((1.0+q1)*q2 - (1.0-q1)*q3))) + 1.0)
+    #     if self.etype == 'GEO':
+    #         return math.floor(final)
+    #     else:
+    #         return int(round(final))
 
-        for i in range(1, self.dimension):
-            for j in range(1, self.dimension):
+    def get_weight(self, first: int, sec: int):
+        if self.etype == 'GEO':
+            RRR = 6378.388
+            q1 = math.cos(self.nodes[first][1] - self.nodes[sec][1])
+            q2 = math.cos(self.nodes[first][0] - self.nodes[sec][0])
+            q3 = math.cos(self.nodes[first][0] + self.nodes[sec][0])
+            return math.floor((RRR*math.acos((0.5*((1.0+q1)*q2 - (1.0-q1)*q3))) + 1.0))
+        else:
+            return math.sqrt((self.nodes[sec][1]-self.nodes[first][1])**2 + (self.nodes[sec][0]-self.nodes[first][0])**2)
+
+    def calculateAdjMatrix(self):
+        for i in range(self.dimension+1):
+            self.adjMatrix.append([0 for i in range(self.dimension+1)])
+
+        for i in range(1, self.dimension+1):
+            for j in range(1, self.dimension+1):
                 self.adjMatrix[i][j] = self.get_weight(i, j)
                 self.adjMatrix[j][i] = self.adjMatrix[i][j]

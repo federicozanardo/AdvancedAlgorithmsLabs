15bef9947ba7491c67763d44f9f239e8cf3b7a06
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-05-13 18:57:58 +0200

    Update Held and Karp, refactor and simplified calculus

--- ex-lab-2/app/algorithms/hk.py
+++ ex-lab-2/app/algorithms/hk.py
@@ -1,10 +1,7 @@
 from collections import defaultdict
-import copy
 import sys
 sys.path.append('../')
-from data_structures.graph import Graph
 from data_structures.tsp import TSP
-from data_structures.heap import Heap, Node
 
 
 
@@ -16,63 +13,48 @@ class HeldKarp:
         self.tsp = None
 
     def _keygen(self, v, S):
-        o = str(v) + ",["
-        for i in S:
-            o = o + str(i) + " "
+        o = str(v) + ",[ "
+        for x in S:
+            o = o + str(x) + " "
         o = o + "]"
         return o
 
-    # graph = struttura dati del grafo
     def hk_init(self, tsp):
-        self.tsp = copy.deepcopy(tsp)
+        self.tsp = tsp
         S = []
-        #print(self.tsp.etype)
         for i in range(1, self.tsp.dimension+1):
             S.append(i)
-            #self.d[i] = {"w": None}
-            self.p[i].append(None)
-        # print(S)
-        return self.hk_visit(1, S)# eh no, manca 1, no ok è giusto PERCHÉ PYTHON GIUSTO
-        
-        # OK FORSE HO CAPITO: NON STA COPIANDO BENE LA LISTA PERCHÉ STA COSA NON È NORMALE
-        # DOVREBBE ANDARE IN RICORSIONE E INVECE TOGLIE TUTTO E POI RIMANE VUOTA
-        # kk e se fosse così sarebbe bene o male? Nel senso, è un problema "del cazzo" o bisogna ripensare tutto?
-        # VUOL DIRE CHE DEEPCOPY È FOTTUTAMENTE ROTTO
-        # Non mi stupisce
-         
+        return self.hk_visit(1, S)
         
     # PRE: S sottoinsieme di V, v € S
-    # POST: d[v,S] è il peso / distanza del cammino minimo da 0 a v che visita tutti i vertici in S, p[v,S] è il predecessore di v
+    # POST: d[v,S] è il peso / distanza del cammino minimo da 0 che visita tutti i vertici in S, 
+    #       p[v,S] è il predecessore di v
     # S = vertici da scorrere presenti in V
     # v = vertice corrente
     def hk_visit(self, v, S):
-        # print("Visiting: ", v, "Remaining: ", len(S))
-        print(self._keygen(v, S))
+        
+        currentKey = self._keygen(v, S)
+        # print(currentKey)
         
         # Caso base 1: soluzione è il peso dell'arco {v, 1}
         if len(S) == 1 and v == S[0]:
-            #print("i came: ", self.tsp.adjMatrix[v][1])
             return self.tsp.adjMatrix[v][1]
 
         # Caso base 2: se il peso è già stato calcolato ritorno il peso
-        elif self._keygen(v, S) in self.d.keys():
-            return self.d[self._keygen(v, S)]
+        elif currentKey in self.d.keys():
+            return self.d[currentKey]
 
         # Caso ricorsivo: cerco il cammino minimo tra tutti i sottocammini 
         else:
             mindist = float('inf')
             minprec = None
-            # Ottengo SS = S \ {v}
-            #SS = copy.deepcopy(S)
-            #SS.remove(v)
             
+            # Ricavo insieme SS = S \ {v}
+            # ndr: più veloce rispetto a una deepcopy, complessità O(n-1)
             SE = []
-
             for x in S:
                 if x != v:
                     SE.append(x)
-
-            # print(SE)
             
             for u in SE:
                 
@@ -88,6 +70,6 @@ class HeldKarp:
                     minprec = u
          
             # Assegno il nuovo peso calcolato che risulta il cammino minimo
-            self.d[self._keygen(v, S)] = mindist 
-            self.p[v] = minprec
+            self.d[currentKey] = mindist 
+            self.p[currentKey] = minprec
             return mindist
